f892d184b60c998f0b54164a21e948e140936eed
zarakikenpashi <sadiarog.abel@gmail.com>
2025-05-10 14:59:04 +0000

    footer layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,5 @@
 @section('title')
 
 @section('content')
-    <h1 class="text-2xl font-bold mb-4">À propos du Cocody Rugby Club</h1>
-    <p>Nous mettons le rugby dans le cœur des jeunes depuis 2015...</p>
+
 @endsection
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,27 @@
-<footer class="bg-gray-900 text-white text-center py-4">
-    <p>&copy; {{ date('Y') }} Cocody Rugby Club. Tous droits réservés.</p>
+<footer class="footer">
+    <div class="footer__container">
+        <div class="footer_logo">
+            <img src="{{ Vite::asset('resources/images/logo.png') }}" alt="" width="100" height="100" style="object-fit:cover">
+        </div>
+        <div class="footer_nav">
+            <ul>
+                <a href="">Le club</a>
+                <a href="">Equipes</a>
+                <a href="">Partenaires</a>
+                <a href="">Agenda</a>
+                <a href="">Actaulités</a>
+                <a href="">Contacts</a>
+            </ul>
+        </div>
+        <div class="footer_legal">
+            <ul>
+                <a href="">© 2025 Club Rubgy Cocody Abidjan</a>
+                <a href="">Conditions générales</a>
+                <a href="">Conditions d'utilisation</a>
+                <a href="">Privacy</a>
+                <a href="">Cookies</a>
+                <a href="">Préférences concernant les cookies</a>
+            </ul>
+        </div>
+    </div>
 </footer>
